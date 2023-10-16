b495d0c1d10b98ac342383f98c391de6dae90fd9
StinkyJeans <johncona3@gmail.com>
2023-10-17 01:39:55 +0800

    added header.js and menuitem.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,31 @@
+import Header from '@/components/header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'IMDB',
-  description: 'This is IMDB clone website',
+  title: 'IMDb',
+  description: 'This is IMDb clone website',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body>
+      {/* Header */}
+      <Header />
+
+      {/* Nav Bar */}
+
+      {/* SearchBox */}
+
+
+      {children}
+        
+        
+        
+      </body>
+    </html> 
   )
 }
