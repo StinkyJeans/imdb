29db896df5ef19c7da67316741ed6b69bcb77dc2
StinkyJeans <johncona3@gmail.com>
2023-10-17 11:42:25 +0800

    added about and page.js

---
+++ src/app/about/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+
+    <h1 className='text-2xl font-medium text-amber-600 '>About</h1>
+
+    <p>
+    Welcome to my website, the ultimate destination for movie enthusiasts, cinephiles, and anyone passionate about the world of cinema. Our mission is to bring you the most comprehensive and up-to-date information on all things related to movies.
+    </p>
+    <p>
+    IMDB Clone is more than just a website; it's a labor of love from a group of dedicated film buffs and professionals who share a profound passion for the art of storytelling through motion pictures. Our team is composed of movie critics, filmmakers, writers, and tech experts, all united by a common love for the magic that happens on the silver screen.
+    </p>
+
+
+
+
+
+
+    </div>
+  )
+}
