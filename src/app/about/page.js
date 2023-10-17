4df295150db8e966f99dbd9f4b6f23b4bf7f3da7
StinkyJeans <johncona3@gmail.com>
2023-10-18 02:09:28 +0800

    some changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,7 +10,7 @@ export default function page() {
     Welcome to my website, the ultimate destination for movie enthusiasts, cinephiles, and anyone passionate about the world of cinema. Our mission is to bring you the most comprehensive and up-to-date information on all things related to movies.
     </p>
     <p>
-    IMDB Clone is more than just a website; it's a labor of love from a group of dedicated film buffs and professionals who share a profound passion for the art of storytelling through motion pictures. Our team is composed of movie critics, filmmakers, writers, and tech experts, all united by a common love for the magic that happens on the silver screen.
+    IMDB Clone is more than just a website; it&apos;s a labor of love from a group of dedicated film buffs and professionals who share a profound passion for the art of storytelling through motion pictures. Our team is composed of movie critics, filmmakers, writers, and tech experts, all united by a common love for the magic that happens on the silver screen.
     </p>
 
 
--- src/app/error.js
+++ src/app/error.js
@@ -1,13 +1,8 @@
 "use client";
 
-import { useEffect } from 'react';
 import React from 'react'
 
 export default function error({error, reset}) {
-
-    useEffect(() => {
-        console.log(error);
-    }, [error]);
   return (
     <div className='text-center mt-10'>
         <h1>Something went wrong</h1>
